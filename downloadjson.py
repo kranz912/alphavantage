36b30c60da731c882d06d9f53f6f7ad2eee32a94
kranz nikolai fontanilla <me@kranznikolai.com>
2020-01-08 23:44:04 -0500

    update

--- downloadjson.py
+++ downloadjson.py
@@ -8,7 +8,7 @@ symbol =  sys.argv[1]
 
 if symbol != None:
 
-    apikey = '38JSBGW4H2GJ8S6A'
+    apikey = ''
 
 
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
